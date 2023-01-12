c01d44217a9cde0419ce7df53e3a76f549cb1e3e
muflikhandimasd <muflikhan.dimas@gmail.com>
2023-01-12 11:01:20 +0300

    Siswa Create

--- app/Http/Controllers/API/SiswaController.php
+++ app/Http/Controllers/API/SiswaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class SiswaController extends Controller
 {
@@ -17,5 +18,27 @@ class SiswaController extends Controller
                 'data' => $siswas,
             ]
         );
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        $rules = [
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'umur' => 'required',
+            'alamat' => 'required',
+        ];
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $validator->errors()
+                ],
+                400
+            );
+        }
     }
 }
